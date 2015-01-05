34831c250404bc8591aa7b0fc2c3258df4123fb4
Jürgen Weigert <jnweiger@gmail.com>
2015-01-05 20:18:07 +0100

    Added copyright header and MODULE_DESCR. Introduced symbolic SYS_REG_* constants. Chosen 152 as a fixed WS2812_MAJOR (to avoid dead device nodes). Silenced most warnings. Prefixed all printk with DEVICE_NAME.

--- src/ws2812-draiveris.c
+++ src/ws2812-draiveris.c
@@ -1,5 +1,15 @@
 /*  
  *  ws2812-draiveris.c - A bitbang kernel module for ws2812
+ *
+ *  Copyright (C) 2006-2012 OpenWrt.org
+ *  Copyright (C) 2012 Žilvinas Valinskas, Saulius Lukšė
+ *  Copyright (C) 2014 Jürgen Weigert <jw@owncloud.com>
+ *
+ *  This is free software, licensed under the GNU General Public License v2.
+ *  See /LICENSE for more information.
+ * 
+ * parameters: 
+ *	gpio_number=20		# set base number. 
  */
 #include <linux/init.h>
 #include <linux/module.h>	/* Needed by all modules */
@@ -26,6 +36,7 @@
 #include <asm/uaccess.h>  /* for put_user */
 #include <linux/fs.h>
 
+#define WS2812_MAJOR		152	/* use a LOCAL/EXPERIMENTAL major for now */
 
 #define sysRegRead(phys) \
  (*(volatile u32 *)KSEG1ADDR(phys)) 
@@ -33,103 +44,117 @@
 #define sysRegWrite(phys, val) \
  ((*(volatile u32 *)KSEG1ADDR(phys)) = (val)) 
 
+// FROM http://www.eeboard.com/wp-content/uploads/downloads/2013/08/AR9331.pdf p65, p77
+#define SYS_REG_GPIO_OE		0x18040000L
+#define SYS_REG_GPIO_IN		0x18040004L
+#define SYS_REG_GPIO_OUT	0x18040008L
+#define SYS_REG_GPIO_SET	0x1804000CL
+#define SYS_REG_GPIO_CLEAR	0x18040010L
+#define SYS_REG_GPIO_INT	0x18040014L
+#define SYS_REG_GPIO_INT_TYPE	0x18040018L
+#define SYS_REG_GPIO_INT_POLARITY	0x1804001CL
+#define SYS_REG_GPIO_INT_PENDING	0x18040020L
+#define SYS_REG_GPIO_INT_MASK		0x18040024L
+#define SYS_REG_GPIO_INT_FUNCTION_1	0x18040028L
+#define SYS_REG_GPIO_IN_ETH_SWITCH_KED	0x1804002CL
+#define SYS_REG_GPIO_INT_FUNCTION_2	0x18040030L
+
+#define SYS_REG_RST_WATCHDOG_TIMER 0x1806000CL
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Saulius Lukse saulius.lukse@gmail.com");
-MODULE_DESCRIPTION("test");
+MODULE_AUTHOR("Saulius Lukse saulius.lukse@gmail.com; Juergen Weigert juewei@fabfolk.com");
+MODULE_DESCRIPTION("Bitbang GPIO driver for multiple WS2812 led chains");
+MODULE_ALIAS_CHARDEV_MAJOR(WS2812_MAJOR);
 
-
-//static char *mystring = "blah";
-//module_param(mystring, charp, 0000);
-//MODULE_PARM_DESC(mystring, "A character string");
 
 static int gpio_number = 20; // default is nr 20
 module_param(gpio_number, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(gpio_number, "GPIO number");
 
+#define SET_GPIOS_H(gpio_bits)	sysRegWrite(SYS_REG_GPIO_SET, gpio_bits)
+#define SET_GPIOS_L(gpio_bits)	sysRegWrite(SYS_REG_GPIO_CLEAR, gpio_bits)
 
 void led_bit(int bit)
 {
   if (bit==0)
   {
     // high (3.3us)
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
 
     // low (7us)
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
 
-    //sysRegWrite(0x1804000CL, 1<<gpio_number);
+    //SET_GPIOS_H( 1<<gpio_number);
   }
   else
   {
-    // high (3.3us)
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
-    sysRegWrite(0x1804000CL, 1<<gpio_number);
+    // high (7us)
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
+    SET_GPIOS_H( 1<<gpio_number);
 
-    // low (7us)
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
-    sysRegWrite(0x18040010L, 1<<gpio_number);
+    // low (3.3us)
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
 
-    //sysRegWrite(0x1804000CL, 1<<gpio_number);
+    //SET_GPIOS_H( 1<<gpio_number);
   }
 }
 
 
 
-void update_leds(char *buff, size_t len)
+void update_leds(const char *buff, size_t len)
 {
 
   unsigned long flags;
   long int i = 0;
   int b = 0;
 
-  sysRegWrite(0x1806000CL, 1<<31); // Just in case set watchdog to timeout some time later 
-  sysRegWrite(0x18040000L, 1<<gpio_number); // output enable to PIN20
+  static DEFINE_SPINLOCK(critical);
 
-  //static DEFINE_SPINLOCK(critical);
+  sysRegWrite(SYS_REG_RST_WATCHDOG_TIMER, 1<<31); // Just in case set watchdog to timeout some time later
+  i = sysRegRead(SYS_REG_GPIO_OE);
+  sysRegWrite(SYS_REG_GPIO_OE, i|1<<gpio_number); 	// output enable to PIN20
 
-  sysRegWrite(0x1804000CL, 1<<gpio_number);
   // wait 65uS
   for(i=0;i<1000;i++)
   {
     volatile int j = i;
-    sysRegWrite(0x18040010L, 1<<gpio_number);
+    SET_GPIOS_L( 1<<gpio_number);
   }
 
 
-  static DEFINE_SPINLOCK(critical);
   spin_lock_irqsave(&critical, flags);
   for (i = 0; i<len; i++)
   {
@@ -139,8 +164,7 @@ void update_leds(char *buff, size_t len)
     else
         led_bit(0);
   }
-  sysRegWrite(0x18040010L, 1<<gpio_number);
-  //sysRegWrite(0x1804000CL, 1<<gpio_number);
+  SET_GPIOS_L( 1<<gpio_number);
   spin_unlock_irqrestore(&critical, flags);
 
 }
@@ -164,7 +188,6 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
  * Global variables are declared as static, so are global within the file. 
  */
 
-static int Major;   /* Major number assigned to our device driver */
 static int Device_Open = 0; /* Is device open?  
          * Used to prevent multiple access to device */
 static char msg[BUF_LEN]; /* The msg the device will give when asked */
@@ -180,15 +203,15 @@ static struct file_operations fops = {
 
 int init_module(void)
 { 
-  Major = register_chrdev(0, DEVICE_NAME, &fops);
-
-  if (Major < 0) {
-    printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-    return Major;
+  int r;
+  if ((r = register_chrdev(WS2812_MAJOR, DEVICE_NAME, &fops))) {
+    printk(KERN_ALERT DEVICE_NAME ": Registering char device major=%d failed with %d\n", WS2812_MAJOR, r);
+    return r;
   }
 
-	printk(KERN_INFO "Major = %d\n", Major);
-  printk(KERN_INFO "GPIO number: %d\n", gpio_number);
+  printk(KERN_INFO DEVICE_NAME ": Build: %s %s\n", __DATE__, __TIME__);
+  printk(KERN_INFO DEVICE_NAME ": Major=%d  device=/dev/%s\n", WS2812_MAJOR, DEVICE_NAME);
+  printk(KERN_INFO DEVICE_NAME ": GPIO number: %d\n", gpio_number);
 
 
 	return SUCCESS;
@@ -200,8 +223,8 @@ int init_module(void)
  */
 void cleanup_module(void)
 {  
-  unregister_chrdev(Major, DEVICE_NAME);
-  printk(KERN_ALERT "That's all\n");
+  unregister_chrdev(WS2812_MAJOR, DEVICE_NAME);
+  printk(KERN_ALERT DEVICE_NAME": Bye, that's all.\n");
 }
 
 
@@ -296,6 +319,6 @@ static ssize_t device_read(struct file *filp, /* see include/linux/fs.h   */
 static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
   update_leds(buff, len);
-  //printk(KERN_INFO "GOT: %s, %d\n", buff, len);
+  //printk(KERN_INFO DEVICE_NAME": GOT: %s, %d\n", buff, len);
   return len;
 }
